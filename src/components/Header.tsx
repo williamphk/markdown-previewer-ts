e575775f990fdc89f57b220836ee8bd30cd08133
William Poon <willphk@outlook.com>
2023-08-31 16:00:28 -0400

    Created Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  border-bottom: 1px solid #e0e0e0;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  color: #333;
+  margin: 0;
+`;
+
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+interface HeaderProps {
+  onToggleGuide: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ onToggleGuide }) => {
+  return (
+    <HeaderContainer>
+      <Title>Markdown Previewer</Title>
+      <Button onClick={onToggleGuide}>Toggle Guide</Button>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
